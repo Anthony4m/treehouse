b24c63596b564161aaa8388f9afa41a5826f1fd8
Anthony4m <amoahanthony41@gmail.com>
2023-02-10 18:14:40 +0000

    added enums and structs

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,35 @@
 use std::io::stdin;
 
 fn main() {
-    let visitors_list = [
-        Visitor::new("kwame", "Hello kwame , how are you?"),
-        Visitor::new("kojo", "Hello kojo , what's up?"),
-        Visitor::new("kwesi", "Hi kwesi , welcome back"),
+    let mut visitors_list = vec![
+        Visitor::new("kwame", VisitorAction::Accept,45),
+        Visitor::new("kojo", VisitorAction::AcceptWithNote {
+            note: String::from("Welcome to the vip section")},20),
+        Visitor::new("kwesi",  VisitorAction::Refuse, 30),
     ];
-    println!("Hello, what is your name?");
-    let your_name = whats_your_name();
-    let known_visitor = visitors_list.iter().find(|visitor| visitor.name == your_name);
-    match known_visitor {
-        Some(visitor) => visitor.greet_visitor(),
-        None => println!("You are not on the visitor's list"),
+    loop {
+        println!("Hello, what is your name?");
+        let your_name = whats_your_name();
+        let known_visitor = visitors_list
+            .iter()
+            .find(|visitor| visitor.name == your_name);
+        match known_visitor {
+            Some(visitor) => visitor.greet_visitor(),
+            None => {
+                if your_name.is_empty() {
+                    println!("The final list of visitors");
+                    println!("{visitors_list:#?}", );
+                    break;
+
+                }
+                println!("You are not on the visitor's list");
+                let probator_age = probation_user_age();
+                visitors_list.push(Visitor::new(&your_name, VisitorAction::Probation, probator_age));
+                println!("{visitors_list:#?}", );
+
+            }
+        }
+
     }
 }
 
@@ -23,19 +41,50 @@ fn whats_your_name() -> String {
         .expect("Failed to read line");
     username.trim().to_lowercase()
 }
-
+fn probation_user_age() -> u8 {
+    println!("What is your age?");
+    let mut your_age =  String::new();
+    stdin()
+        .read_line(&mut your_age)
+        .expect("Failed to read line");
+        your_age.trim().parse::<u8>().unwrap()
+}
+#[derive(Debug)]
 struct Visitor {
     name: String,
-    greeting: String,
+    action: VisitorAction,
+    age: u8,
 }
 impl Visitor {
-    fn new(name: &str, greeting: &str) -> Self {
+    fn new(name: &str,action:VisitorAction,age:u8) -> Self {
         Self {
             name: name.to_lowercase(),
-            greeting: greeting.to_string(),
+            action,
+            age
         }
     }
     fn greet_visitor(&self) {
-        println!("{}", self.greeting);
+        match &self.action {
+            VisitorAction::Accept => println!("Welcome to treehouse, {0}", self.name),
+            VisitorAction::AcceptWithNote {note} => {
+                println!("Welcome to treehouse, {0}", self.name);
+                println!("{note}");
+                if self.age <= 21 {
+                    println!("You cant have alcohol");
+                }
+            }
+            VisitorAction::Probation => {
+                println!("Welcome to treehouse, {0}, Note you are on probation", self.name);
+            },
+            VisitorAction::Refuse => println!("You are not welcome, {0}", self.name),
+        }
     }
+}
+#[derive(PartialEq)]
+#[derive(Debug)]
+enum VisitorAction {
+    Accept,
+    AcceptWithNote { note: String },
+    Refuse,
+    Probation,
 }
